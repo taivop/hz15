1d02f030200205ae892fa4214e860d8b086b67d6
taivop <taivo.pungas@gmail.com>
2015-10-03 16:28:41 +0200

    working node app with database connection

--- node_server/app.js
+++ node_server/app.js
@@ -1,29 +1,91 @@
-/*eslint-env node*/
+var port = (process.env.VCAP_APP_PORT || 3000),
+    host = (process.env.VCAP_APP_HOST || 'localhost'),
+    pg = require('pg.js'),
+    express = require('express'),
+    app = express(),
+    psql;
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+if (process.env.VCAP_SERVICES) {
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  psql = env['postgresql-9.1'][0].credentials;
+  // The Postgresql service returns the database name in the
+  "name"
+  // property but the pg.js Node.js module expects it to be in
+  the
+  // "database" property.
+  psql.database = psql.name;
+} else {
+  // Specify local Postgresql connection properties here.
+  psql = {
+    database: "postgres",
+    host: "localhost",
+    port: 5432,
+    user: "",
+    password: "",
+    ssl: false
+  };
+}
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+// Add the visit to the database and display the number of visits for the
+// requesting ip address.
+    var record_visit = function(req, res) {
+      /* Connect to the DB and auth */
+      pg.connect(psql, function(err, client, done) {
+        if (err) {
+          return console.error('Error requesting client', err);
+        }
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+        client.query('insert into ips(ip, ts) values($1, $2)',
+            [req.connection.remoteAddress, new Date()],
+            function(err, result) {
+              if (err) {
+                done();
+                return console.error('Error inserting ip', err);
+              }
 
-// create a new express server
-var app = express();
+              client.query('select count(ip) as count from ips where ip=$1',
+                  [req.connection.remoteAddress], function(err,
+                                                           result) {
+                done();
+                if (err) {
+                  return console.error('Error querying count',
+                      err);
+                }
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end("You have visited " + result.rows[0].count
+                    + " times");
+              });
+            });
+      });
+    };
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+// On startup, drop the table if it exists then create it.
+pg.connect(psql, function(err, client, done) {
+  if (err) {
+    return console.error('Error requesting client', err);
+  }
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+  client.query('drop table if exists ips', function(err, result)
+  {
+    if (err) {
+      done();
+      return console.error('Error dropping table ips', err);
+    }
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, function() {
-
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+    client.query('create table ips(id serial, ip varchar(40), ts date)',
+    function (err, result) {
+      done();
+      if (err) {
+        return console.error('Error creating table ips',
+            err);
+      }
+    });
+  });
 });
+
+app.get('/', function(req, res) {
+  record_visit(req, res);
+});
+
+console.log("Listening on " + host + ":" + port);
+app.listen(port, host);
